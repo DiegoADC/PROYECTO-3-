ac0c8eed949932a190f945c7b2e91a18b0afcab7
DiegoADC <diego35502@hotmail.com>
2022-05-28 00:36:47 -0500

    responsive fixed

--- Script/app.js
+++ Script/app.js
@@ -1,5 +1,6 @@
 const nav = document.querySelector('#hamburger');
 const menu = document.querySelector('#menu');
+const menuClose = document.getElementById('menu-info')
 // Menu Hamburguesa
 
 nav.addEventListener('click', e =>{
@@ -8,6 +9,10 @@ nav.addEventListener('click', e =>{
 // Menu lateral
 nav.addEventListener('click', (e) => {
     menu.classList.toggle('menu-active');
+})
+menuClose.addEventListener('click', (e)=>{
+    menu.classList.remove('menu-active');
+    nav.classList.remove('open');  
 })
 
 

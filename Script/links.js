bf5b2e5bffc5852a6bca3a87fa8cf5ed596f936a
DiegoADC <diego35502@hotmail.com>
2022-08-13 09:41:04 -0500

    new credentials

--- Script/links.js
+++ Script/links.js
@@ -3,5 +3,5 @@ export const urlFollows = 'https://api.twitch.tv/helix/users/follows?to_id='
 export const urlUsers = 'https://api.twitch.tv/helix/users?login='
 export const urlStreams = 'https://api.twitch.tv/helix/streams?user_login=';
 export const urlTwitch = 'https://www.twitch.tv/'
-export const token = { method: "GET", headers: {"Authorization": "Bearer s2t89l66iaso9cr6g0kyin3ovebl0i", "Client-Id": "9evi49kf49azcz9t4si807u6e0sban" }};
+export const token = { method: "GET", headers: {"Authorization": "Bearer nlzqvt3arf96rrptu5zfdp68jsrsvl", "Client-Id": "9evi49kf49azcz9t4si807u6e0sban" }};
 export const linkJson = '../json/list-streamer.json';
